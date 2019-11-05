5a7cabbf167fc3598d7ba67a12b458d42c2be2c3
Guilherme <guih.miranda@hotmail.com>
2019-11-05 15:25:53 -0300

    ping sweep

--- pingscript.sh
+++ pingscript.sh
@@ -4,7 +4,7 @@ then
 echo "usage: ./pingscript.sh [network]"
 echo "example: ./pingscript.sh 172.26.4"
 else
-for x in `seq 110 130`; do
+for x in `seq 1 254`; do
 ping -c 3 $1.$x > /dev/null 2>&1 && echo $1.$x está up! || echo $1.$x está down! 
 done
 fi
